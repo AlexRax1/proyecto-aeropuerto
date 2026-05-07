5e574fc6708718dde2fdf548058d0659d7b36796
Helmuth Marroquin <helmuthmarro@gmail.com>
2026-05-06 22:01:30 -0600

    Avance Proyecto 00

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AvionSelectionComponent } from './pages/avion-selection/avion-selection.component';
+import { SeatSelectionComponent } from './pages/seat-selection/seat-selection.component';
+import { PaymentComponent } from './pages/payment/payment.component';
+import { ConfirmationComponent } from './pages/confirmation/confirmation.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'aviones',
+    component: AvionSelectionComponent
+  },
+  {
+    path: 'asientos',
+    component: SeatSelectionComponent
+  },
+  {
+    path: 'pago',
+    component: PaymentComponent
+  },
+  {
+    path: 'confirmacion',
+    component: ConfirmationComponent
+  }
+];
---
+++ frontend/src/app/pages/avion-selection/avion-selection.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-avion-selection',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './avion-selection.component.html',
+  styleUrls: ['./avion-selection.component.css']
+})
+export class AvionSelectionComponent {
+
+}
